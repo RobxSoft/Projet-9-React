352e4cce96cf8aa2948a36a28e4922627cd3c877
Falaron <arthurjouve91@gmail.com>
2022-03-03 16:03:40 +0100

    finishing nav and footer

--- app-react/src/components/Footer.js
+++ app-react/src/components/Footer.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 
 import 'materialize-css/dist/css/materialize.min.css';
 
-import logo from '../img/Logo_Title.png'
+import logo from '../img/Logo_Title.png';
+import InstaLogo from '../img/instagram.png';
+import LinkedinLogo from '../img/linkedin.png';
+import TwitterLogo from '../img/twitter.png';
+import FbLogo from '../img/facebook.png';
 
 class Footer extends React.Component {
 
@@ -14,24 +18,30 @@ class Footer extends React.Component {
                         <div className="row">
                             <div className="col l4 s12">
                                 <ul>
-                                <li><a className="grey-text text-lighten-3" href="#!">Legal Notice</a></li>
-                                <li><p className="grey-text text-lighten-3" href="#!">Contact Us</p></li>
-                                <li><p className="grey-text text-lighten-3" href="#!">(+33)6 00 00 00 00</p></li>
-                                <li><p className="grey-text text-lighten-3" href="#!">contact@fireball.com</p></li>
+                                <li className="is-footer"><a className="grey-text text-lighten-3" href="#!">Legal Notice</a></li>
+                                <li className="is-footer"><p className="grey-text text-lighten-3" href="#!">Contact Us</p></li>
+                                <li><p href="#!" className="color">(+33)6 00 00 00 00</p></li>
+                                <li><p href="#!" className="color">contact@fireball.com</p></li>
                                 </ul>
                             </div>
                             <div className="col l4 s12">
                                 <img className="responsive-img" src={logo} />
                                 <ul>
-                                <li><a className="grey-text text-lighten-3" href="#!">Home</a></li>
-                                <li><a className="grey-text text-lighten-3" href="#!">Discover</a></li>
-                                <li><a className="grey-text text-lighten-3" href="#!">Articles</a></li>
+                                <li className="is-footer color"><a href="#!">Home</a></li>
+                                <li className="is-footer color"><a href="#!">Discover</a></li>
+                                <li className="is-footer color"><a href="#!">Articles</a></li>
                                 </ul>
+                                <div className="social-network">
+                                    <a href="!#"><img src={InstaLogo}/></a>
+                                    <a href="!#"><img src={LinkedinLogo}/></a>
+                                    <a href="!#"><img src={TwitterLogo}/></a>
+                                    <a href="!#"><img src={FbLogo}/></a>
+                                </div>
                             </div>
                             <div className="col l4 s12">
                                 <ul>
-                                <li><a className="grey-text text-lighten-3" href="#!">Terms of Service</a></li>
-                                <li><a className="grey-text text-lighten-3" href="#!">Privacy Policy</a></li>
+                                <li className="is-footer"><a className="grey-text text-lighten-3" href="#!">Terms of Service</a></li>
+                                <li className="is-footer"><a className="grey-text text-lighten-3" href="#!">Privacy Policy</a></li>
                                 </ul>
                             </div>
                         </div>
