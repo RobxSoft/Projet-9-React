2170fda3e28fca4f9981ca40face14a8775f6ba8
RobxSoft <48138370+RobxSoft@users.noreply.github.com>
2022-03-07 14:35:52 +0100

    stating game page

--- app-react/src/views/Games.js
+++ app-react/src/views/Games.js
@@ -13,7 +13,7 @@ class Games extends React.Component {
                 <div className="container-games row">
                     <h2 className="flow-text">Popular Games</h2>
                     <a class="waves-effect btn z-depth-0">SEE ALL</a>
-                    <div className="send-game-container">
+                    <div className="game-container">
                         <ContainerPopular/>
                         <ContainerPopular/>
                         <ContainerPopular/>
