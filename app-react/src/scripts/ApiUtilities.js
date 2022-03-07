ab07f370d2e24b26559e1b0c5b4f0c18b0fcc96f
Falaron <arthurjouve91@gmail.com>
2022-03-07 13:28:29 +0100

    link bdd 2

--- app-react/src/scripts/ApiUtilities.js
+++ app-react/src/scripts/ApiUtilities.js
@@ -1,3 +1,5 @@
 export const getArticle = async () => {
-
+    const response = await fetch('http://localhost:3001/articles', {method: 'GET', headers: {'Accept': 'application/json', 'Content-Type':'application/json'}})
+    const articles = await response.json()
+    return articles
 }
