68c8ed5246d57530c36a8f753f8800532086927b
Falaron <arthurjouve91@gmail.com>
2022-03-09 23:49:58 +0100

    salade de riz

--- app-react/src/views/ArticleInfo.js
+++ app-react/src/views/ArticleInfo.js
@@ -67,13 +67,11 @@ class ArticleInfo extends React.Component {
                     </div>
                 </div>
 
-
                 <div className="container-games">
-                        <h2 className="flow-text">Dedicated Articles</h2>
-                        <a className="waves-effect btn z-depth-0">SEE ALL</a>
-                        <div className="send-game-container">
-                            
-                        </div>
+                    <h2 className="flow-text">Dedicated Articles</h2>
+                    <a className="waves-effect btn z-depth-0">SEE ALL</a>
+                    <div className="send-game-container">
+                    </div>
                 </div>
                 <Footer />
             </React.Fragment>
